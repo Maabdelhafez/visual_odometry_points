abe415797e90193ae43d1c837452aa132fe39fa8
Sherman Chen <schen@simviu.com>
2022-07-13 10:46:37 +0800

    build pass

--- back/vop_bk1.cpp
+++ back/vop_bk1.cpp
@@ -4,7 +4,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <Eigen/Core>
-/*
 #include <g2o/core/base_vertex.h>
 #include <g2o/core/base_unary_edge.h>
 #include <g2o/core/sparse_optimizer.h>
@@ -12,15 +11,13 @@
 #include <g2o/core/solver.h>
 #include <g2o/core/optimization_algorithm_gauss_newton.h>
 #include <g2o/solvers/dense/linear_solver_dense.h>
-*/
-//#include <sophus/se3.hpp>
-#include <sophus/se3.h>
+#include <sophus/se3.hpp>
 #include <chrono>
-//#include <opencv2/imgcodecs/legacy/constants_c.h>
+#include <opencv2/imgcodecs/legacy/constants_c.h>
 #include <unistd.h>
 #include <vector>
 #include <string>
-//#include <pangolin/pangolin.h>
+#include <pangolin/pangolin.h>
 
 
 using namespace std;
@@ -55,10 +52,10 @@ Point2d pixel2cam(const Point2d &p, const Mat &K);
 typedef vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>> VecVector2d;
 typedef vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> VecVector3d;
 
-//void bundleAdjustmentG2O(const VecVector3d &points_3d, const VecVector2d &points_2d, const Mat &K, Sophus::SE3d &pose);
+void bundleAdjustmentG2O(const VecVector3d &points_3d, const VecVector2d &points_2d, const Mat &K, Sophus::SE3d &pose);
 
 // BA by gauss-newton
-//void bundleAdjustmentGaussNewton(const VecVector3d &points_3d, const VecVector2d &points_2d, const Mat &K, Sophus::SE3d &pose);
+void bundleAdjustmentGaussNewton(const VecVector3d &points_3d, const VecVector2d &points_2d, const Mat &K, Sophus::SE3d &pose);
 
 Mat generateDepthMap(void);
 
